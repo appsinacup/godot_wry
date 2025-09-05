d2a41c017fa543a5a8502c950884ded91492fce9
Dragos Daian <daian.dragos@yahoo.com>
2025-09-05 13:31:20 +0200

    Disable module linux 32 bit builds.

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ def can_build(env, platform):
     if platform == "windows" and env.get("use_mingw", False):
         print("godot_wry: Disabled for Windows MinGW builds")
         return False
+    if platform == "linuxbsd" and env["arch"] == "x86_32":
+        print("godot_wry: Disabled for Linux x86_32 builds (WebKit libraries not available)")
+        return False
     return platform in ["linuxbsd", "windows", "macos"]
 
 def configure(env):
